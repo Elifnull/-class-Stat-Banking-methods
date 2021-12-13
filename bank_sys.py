628f9892944c279aacf0d1adeed26dfb9dcb0598
Serge Chvorun <elifnull@protonmail.com>
2021-12-12 23:13:02 -0500

    class method parsing

--- bank_sys.py
+++ bank_sys.py
@@ -1,12 +1,17 @@
 class Bankaccount:
-    def __init__(self, user_name: str, int_rate = .07, balance = 0):
+    all_accounts = []
+    def __init__(self, user_name, int_rate = .07, balance = 0):
+        #instance level parameters
         self.user_name = user_name
         self.int_rate = int_rate
         self.balance = balance
-        
+
+        #class level parameters
+        Bankaccount.all_accounts.append(self) #singular not plural.... append() not push()
+
         #Assertions & validations
-        assert int_rate > 0
-        assert balance > 0
+        assert balance >= 0
+        assert int_rate >= 0
     
     def deposit(self, amount):
         self.balance += amount
@@ -27,12 +32,19 @@ class Bankaccount:
 
     def yield_intrest(self):
         if self.balance > 0:
-            self.balance *= (1 + (self.balance * self.int_rate))
+            self.balance *= (1 + (self.int_rate))
         else:
             print(f"Current Balance: $ {self.balance} yields no intrest")
         return self
+    @classmethod
+    def see_all(cls):
+        print(f'the eye that sees all sees: {cls.all_accounts}')
 
 
-serge = Bankaccount("Serge", .05, 1000)
+olga = Bankaccount("olga", .07, 1000).deposit(750).deposit(5).deposit(75).withdrawl(775).yield_intrest().display_account_info()
 
-serge.display_account_info() ###the brackets SERGE THE BRACKETSSSS!!!!
+serge = Bankaccount("serge")
+
+serge.deposit(100).deposit(98).withdrawl(20).withdrawl(20).withdrawl(35).withdrawl(40).yield_intrest().display_account_info()
+
+Bankaccount.see_all()
